e02672bc063c6832cbe3e34ed0d6b037a5c5729e
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2022-04-09 20:03:35 -0400

    add theme colors customization

--- src/admin/app.js
+++ src/admin/app.js
@@ -28,6 +28,17 @@ export default {
       // 'zh-Hans',
       // 'zh',
     ],
+    // Override or extend the theme
+    theme: {
+      colors: {
+        primary100: '#f6ecfc',
+        primary200: '#e0c1f4',
+        primary500: '#ac73e6',
+        primary600: '#9736e8',
+        primary700: '#8312d1',
+        danger700: '#b72b1a'
+      },
+    },
   },
   bootstrap(app) {
     console.log(app);
